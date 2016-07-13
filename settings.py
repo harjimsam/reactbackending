d20d62d6dfc88ec426bddcac8d7dcfdb13a2c6e6
jee45 <jee45@txstate.edu>
2016-07-13 13:26:26 -0500

    added postgres url

--- settings.py
+++ settings.py
@@ -2,6 +2,6 @@ SECRET_KEY =  'A0Zr98j/3yX R~XHH!jmN]LWX/,?RT'
 
 ADMIN_PASSWORD = 'admin'
 
-SQLALCHEMY_DATABASE_URI  = 'sqlite:///p33.db'
+SQLALCHEMY_DATABASE_URI  = 'postgres://vgbbklbhhxbnrt:zQZiRRek9ZwPj7A67EwgdKbku_@ec2-23-21-179-195.compute-1.amazonaws.com:5432/d4813228v51lhq'
 
 SQLALCHEMY_TRACK_MODIFICATIONS = False
